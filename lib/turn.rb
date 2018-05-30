283e00567c35f9afec548a08e01adc11d872b6de
Ishani <ishaniamin@gmail.com>
2018-05-30 19:17:19 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,15 +41,15 @@ end
 
 
 
-  def turn(board)
-    puts "Please enter 1-9:"
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.strip
+  until valid_move?(board, position)
+    puts "Wrong number. There is no such position. Input a number from 1 to 9"
     position = gets.strip
-    until valid_move?(board, position)
-      puts "Wrong number. There is no such position. Input a number from 1 to 9"
-      position = gets.strip
-    end
-    move(board, position, x_or_o="X")
-    display_board(board)
   end
+  move(board, position, x_or_o="X")
+  display_board(board)
+end
 
 turn(board)
